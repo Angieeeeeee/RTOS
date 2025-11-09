d2318137d1db7c8e8f13c0f77bd5fd9118caf671
Angieeeeeee <80398192+Angieeeeeee@users.noreply.github.com>
2025-11-09 01:19:58 +0000

    shell code

--- shell.h
+++ shell.h
@@ -15,6 +15,29 @@
 // Subroutines
 //-----------------------------------------------------------------------------
 
+void getsUart0(USER_DATA *data);
+void parseFields(USER_DATA *data);
+bool isCommand(USER_DATA* data, const char strCommand[], uint8_t minArguments);
+char* getFieldString(USER_DATA* data, uint8_t fieldNumber);
+int32_t getFieldInteger(USER_DATA* data, uint8_t fieldNumber);
+bool sameStr(const char str1[], const char str2[]);
+bool isCommand(USER_DATA* data, const char strCommand[], uint8_t minArguments);
+void ps(void);
+void ipcs(void);
+void kill(uint32_t pidK);
+void pkill(char* processName);
+void pi(bool on);
+void preempt(bool on);
+void sched(bool prioOn);
+void pidof(char* name);
+void run(char* name);
+void busFaltTrig(void);
+void usageFaltTrig(void);
+void hardFaltTrig(void);
+void mpuFaltTrig(void);
+void pendsvTrig(void);
+void test1(void);
+void test2(void);
 void shell(void);
 
 #endif
